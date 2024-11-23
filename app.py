2c615679f919f5c74e593cfe44f1a39da6633f86
bharani-sankar <bharanisankar10@gmail.com>
2024-11-24 02:18:15 +0530

    Third commit

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, request, jsonify
 import mysql.connector
 from flask_cors import CORS
+from mysql.connector import Error
 
 app = Flask(__name__)
 CORS(app)
 
 # Configure MySQL connection
-db = mysql.connector.connect(
-    host="sql12.freesqldatabase.com",
-    port=3306,
-    user="sql12746980",
-    password="HDrSwblqmu",
-    database="sql12746980"
-)
+def create_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="sql12.freesqldatabase.com",
+            port=3306,
+            user="sql12746980",
+            password="HDrSwblqmu",
+            database="sql12746980"
+        )
+        if connection.is_connected():
+            return connection
+        else:
+            return None
+    except Error as e:
+        print("Error while connecting to MySQL:", e)
+        return None
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -20,28 +30,47 @@ def login():
     username = data.get('username')
     password = data.get('password')
 
-    # Validate user credentials
-    cursor = db.cursor(dictionary=True)
-    query = """
-        SELECT usertype, blood_group 
-        FROM users 
-        WHERE username=%s AND password=%s
-    """
-    cursor.execute(query, (username, password))
-    user = cursor.fetchone()
-
-    if user:
-        return jsonify({
-            "status": "true",
-            "usertype": user['usertype'],
-            "blood_group": user['blood_group'],  # Include blood_group in the response
-            "message": f"Welcome, {username}!"
-        }), 200
-    else:
+    # Connect to the database
+    db = create_db_connection()
+    
+    if db is None:
         return jsonify({
             "status": "false",
-            "message": "Invalid credentials!"
-        }), 400
+            "message": "Could not connect to database!"
+        }), 500
+
+    try:
+        # Validate user credentials
+        cursor = db.cursor(dictionary=True)
+        query = """
+            SELECT usertype, blood_group 
+            FROM users 
+            WHERE username=%s AND password=%s
+        """
+        cursor.execute(query, (username, password))
+        user = cursor.fetchone()
+
+        if user:
+            return jsonify({
+                "status": "true",
+                "usertype": user['usertype'],
+                "blood_group": user['blood_group'],  # Include blood_group in the response
+                "message": f"Welcome, {username}!"
+            }), 200
+        else:
+            return jsonify({
+                "status": "false",
+                "message": "Invalid credentials!"
+            }), 400
+    except Error as e:
+        return jsonify({
+            "status": "false",
+            "message": f"Database query error: {str(e)}"
+        }), 500
+    finally:
+        if db.is_connected():
+            cursor.close()  # Close cursor
+            db.close()  # Close connection
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
